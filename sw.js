a16659f9a79442e822a5d01bd237ddc0b1862e88
Victor Torres Ortiz <victor.torres.ortiz.18@gmail.com>
2022-11-23 18:20:51 +0100

    Añadiemdo PWA support

---
+++ sw.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const cacheName = "v1";
+
+// Install a service worker
+self.addEventListener("install", (event) => {
+    console.log("Service Workers: Installed");
+});
+
+// Cache and return requests
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .then((res) => {
+                const resClone = res.clone();
+                // Open cache
+                caches.open(cacheName).then((cache) => {
+                    cache.put(event.request, resClone);
+                });
+                return res;
+            })
+            .catch((err) =>
+                caches
+                    .match(event.request)
+                    .then((res) => res)
+                    .catch((err) => console.error(err))
+            )
+    );
+});
+
+// Update a service worker
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== cacheName) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
+
+//Delete old Cache
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.filter(function(cacheName) {
+          }).map(function(cacheName) {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+    );
+  });
