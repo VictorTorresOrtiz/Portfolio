68ea560202ee36b10da15cd8ab23c92a7f19ed39
Victor Torres Ortiz <victor.torres.ortiz.18@gmail.com>
2022-11-25 14:50:05 +0100

    Añadiendo poups

---
+++ js/habilidades.js
@@ -0,0 +1,19 @@
+"use strict";
+function bootstrap(){
+swal("Bootstrap", "Desarrollo Frontend con Boostrap (5 & 4)");
+}
+
+function apps(){
+    swal("Aplicaciones", "Aplicaciones IOS/Android de cualquier temática");
+}
+
+function software(){
+    swal("Desarrollo Software", "Desarrollo de aplicaciones destinada a empresas");
+}
+
+function sistemas(){
+    swal("Sistemas y Redes", "Gestión, mantenimiento, montaje de servidores y equipos de sobremesa");
+}
+    
+    
+    
